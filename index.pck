b925da07c5c729bc4d4378e346eeed270af37905
Mehanth <mehanth2310579@ssn.edu.in>
2025-11-23 00:57:38 +0530

    per tile zoom in and rotation

--- index.pck
+++ index.pck
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6908ad07bce8a61f7f6086a5667ac3ef11e66c4fb83870fa1d86256efb5c31a5
-size 177081536
+oid sha256:5e811cec43e88b191bb0d27e5db54d54905185fde4d6aa82493af94647d876b1
+size 177496032
--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1763836556|2906685357';
+const CACHE_VERSION = '1763839628|2065737285';
 /** @type {string} */
 const CACHE_PREFIX = 'Crown of Soverei-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
